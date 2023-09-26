c824e3c9a637706ac3a7442b54349acce8643618
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-26 13:57:25 +0300

    progress-half-way

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,28 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
+    def __init__(self,name,breed):
+        self.id=None
+        self.name=name
+        self.breed=breed
+
+    @classmethod
+    def create_table(cls):
+        sql="""CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )"""
+        CURSOR.execute(sql)
+        
     
-    pass
+    @classmethod
+    def drop_table(cls):
+        sql="""DROP TABLE IF EXISTS dogs"""
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql="""INSERT INTO dogs (name,breed) VALUES (?,?)"""
+        CURSOR.execute(sql,(self.name,self.breed))
+
+    
